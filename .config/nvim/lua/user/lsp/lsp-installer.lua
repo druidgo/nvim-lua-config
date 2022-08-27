4237f805dfa0ad40894c7bbbaeca51bf4c8f64be
th1rt3en <giuseppe.richetti@gmail.com>
2022-08-27 16:33:58 +0200

    Initial upload

---
+++ .config/nvim/lua/user/lsp/lsp-installer.lua
@@ -0,0 +1,61 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+  return
+end
+
+-- Add installed servers here to actually have them work
+-- :LspInstallInfo to see available servers
+local servers = {
+  "sumneko_lua",
+  "cssls",
+  "html",
+  "tsserver",
+  "pyright",
+  "bashls",
+  "jsonls",
+  "yamlls",
+  "gopls",
+  "golangci_lint_ls",
+  "bashls",
+  "clangd",
+  "cssmodules_ls",
+  "emmet_ls",
+  "eslint",
+  "intelephense",
+  "quick_lint_js",
+  "rome",
+  "rust_analyzer",
+  "solc",
+  "taplo",
+  "terraformls",
+  "tflint",
+  "zk",
+}
+
+lsp_installer.setup()
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+  }
+
+  if server == "sumneko_lua" then
+    local sumneko_opts = require "user.lsp.settings.sumneko_lua"
+    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
+  if server == "pyright" then
+    local pyright_opts = require "user.lsp.settings.pyright"
+    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+  end
+
+  lspconfig[server].setup(opts)
+end
